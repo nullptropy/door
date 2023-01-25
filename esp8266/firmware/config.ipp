6ce1e0a10c4ec3c2e96d24d91d38a6945986cec1
brkp <brkp@tutanota.com>
2023-01-25 18:15:56 +0300

    configuration header

---
+++ esp8266/firmware/config.ipp
@@ -0,0 +1,16 @@
+#ifndef firmware_config_h
+#define firmware_config_h
+
+#include <inttypes.h>
+
+#define RST_PIN D1
+#define SS_PIN1 D2
+#define SS_PIN2 D4
+
+#define SSID                ""
+#define PASS                ""
+#define LOGIN_REMOTE_ADDR   ""
+#define EXIT_REMOTE_ADDR    ""
+#define POST_JSON_STRING    "{\"uid\": %u}"
+
+#endif
--- esp8266/firmware/firmware.ino
+++ esp8266/firmware/firmware.ino
@@ -1,6 +1,3 @@
-#include <stdbool.h>
-#include <inttypes.h>
-
 #include <SPI.h>
 #include <MFRC522.h>
 
@@ -8,71 +5,55 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-#define RST_PIN D1
-#define  SS_PIN D4
+#include "config.ipp"
 
-#define SSID ""
-#define PASS ""
-#define REMOTE_ADDR ""
-
-void led_success_sequence(void);
-void led_failure_sequence(void);
-
-MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522 rfids[2];
 
 void setup() {
-    Serial.begin(9600); while (!Serial);
-    SPI.begin(); rfid.PCD_Init();
+    Serial.begin(9600);
 
     WiFi.begin(SSID, PASS);
-
     while (WiFi.status() != WL_CONNECTED) {
-       delay(500);
-       Serial.print(".");
-    }
-    Serial.println("");
+        delay(500);
+        Serial.print(".");
+    };
+
     Serial.println("WiFi connected");
     Serial.println(WiFi.localIP());
 
-    pinMode(LED_BUILTIN, OUTPUT);
+    SPI.begin();
+    rfids[0].PCD_Init(SS_PIN1, RST_PIN);
+    rfids[1].PCD_Init(SS_PIN2, RST_PIN);
 }
 
 void loop() {
-    if (WiFi.status()== WL_CONNECTED) {
-        WiFiClient client;
-        HTTPClient http;
+    for (int i = 0; i < 2; ++i) {
+        if (rfids[i].PICC_IsNewCardPresent() && rfids[i].PICC_ReadCardSerial()) {
+            if (rfids[i].uid.size != 4)
+                continue;
 
-        http.begin(client, REMOTE_ADDR);
-        http.GET();
+            uint32_t uid = \
+                rfids[i].uid.uidByte[0] << 0x18 |
+                rfids[i].uid.uidByte[1] << 0x10 |
+                rfids[i].uid.uidByte[2] << 0x08 |
+                rfids[i].uid.uidByte[3] << 0x00;
+
+            rfids[i].PICC_HaltA();
+            rfids[i].PCD_StopCrypto1();
+
+            if (WiFi.status() == WL_CONNECTED) {
+                char request_body[20];
+                WiFiClient client;
+                HTTPClient http;
+
+                http.begin(client, i == 0 ? LOGIN_REMOTE_ADDR : EXIT_REMOTE_ADDR);
+                http.addHeader("Content-Type", "application/json");
+
+                snprintf(request_body, sizeof(request_body), "{\"uid\": %zu}", uid);
+                Serial.print("reader: "); Serial.print(i);
+                Serial.print(" request_body: "); Serial.print(request_body);
+                int http_code = http.POST(request_body);
+            }
+        }
     }
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    if (!rfid.PICC_IsNewCardPresent()) return;
-    if (!rfid.PICC_ReadCardSerial()) return;
-
-    uint32_t uid = *(uint32_t *)rfid.uid.uidByte;
-    Serial.println(uid);
-
-    if (uid == 0xA2F28A90) {
-        led_success_sequence();
-    }
-    else {
-        led_failure_sequence();
-    }
-}
-
-void led_success_sequence(void) {
-    for (int i = 0; i < 3; i++) {
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(100);
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-    }
-}
-
-void led_failure_sequence(void) {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
 }
